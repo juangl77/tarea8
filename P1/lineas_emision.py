69d4e486b8bd5eee297ec2ff9aee6d66507ca261
henrymmg <hm.martinez57@uniandes.edu.co>
2014-05-15 11:58:43 -0500

    final punto 1

--- P1/lineas_emision.py
+++ P1/lineas_emision.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import pylab as plt
 from math import *
 
 #Se cargan los datos de las observaciones
@@ -15,7 +15,7 @@ def Likelihood(n_cuen_datos, n_cuen_modelo):
 
 #Modelo fisico
 def mi_modelo(n_ener_datos, A, B, Eo, sigma, alpha):
-    return ( A * (n_ener_datos**alpha) ) + ( B * np.exp(-( (n_ener_datos-Eo) / (np.sqrt(2)*sigma) )**2) )
+    return  A*(n_ener_datos**alpha) + B*np.exp(-((n_ener_datos-Eo)/(np.sqrt(2)*sigma))**2) 
 
 #Listas vacias que guardan los pasos
 paso_A = np.empty((0)) 
@@ -26,29 +26,23 @@ paso_alpha = np.empty((0))
 paso_L = np.empty((0))
 
 #Inicializacion de pasos. Los valores se estimaron tras correr varias veces el programa
-A_o = 10**16
-paso_A = np.append(paso_A, A_o)
-B_o = 10**3
-paso_B = np.append(paso_B, B_o)
-Eo_o = 1.387*10**3
-paso_Eo = np.append(paso_Eo, Eo_o)
-sigma_o = 0
-paso_sigma = np.append(paso_sigma, sigma_o)
-alpha_o = 10**2
-paso_alpha = np.append(paso_alpha, alpha_o)
+paso_A = np.append(paso_A, 5*10**15)
+paso_B = np.append(paso_B, 2*10**3)
+paso_Eo = np.append(paso_Eo, 1400)
+paso_sigma = np.append(paso_sigma, 10**2)
+paso_alpha = np.append(paso_alpha, -4)
 
 n_cuen_inicial = mi_modelo(n_ener_datos, paso_A[0], paso_B[0], paso_Eo[0], paso_sigma[0], paso_alpha[0])
 paso_L = np.append(paso_L, Likelihood(n_cuen_datos, n_cuen_inicial))
 
-n_iteraciones = 100000 
-
-for i in range(n_iteraciones-1):
-    #Se define el paso de cada variable como 3 ordenes de magnitud menos que el valor de inicializacion
+n_iteraciones = 20000 #Se definio aumentando el orden de magnitud hasta obtener la mejor aproximacion
+for i in range(n_iteraciones):
+    #Se define el paso de cada variable unos ordenes de magnitud menos que el valor de inicializacion
     A_prima = np.random.normal(paso_A[i], 10**13) 
-    B_prima = np.random.normal(paso_B[i], 10**0)
-    Eo_prima = np.random.normal(paso_Eo[i], 10**0)
-    sigma_prima = np.random.normal(paso_sigma[i], 10**-2)
-    alpha_prima = np.random.normal(paso_alpha[i], 10**-1)
+    B_prima = np.random.normal(paso_B[i], 10**-1)
+    Eo_prima = np.random.normal(paso_Eo[i], 10**-1)
+    sigma_prima = np.random.normal(paso_sigma[i], 0.01) 
+    alpha_prima = np.random.normal(paso_alpha[i], 0.1)
     
     n_cuen_inicial = mi_modelo(n_ener_datos, paso_A[i], paso_B[i], paso_Eo[i], paso_sigma[i], paso_alpha[i])
     n_cuen_prima = mi_modelo(n_ener_datos, A_prima, B_prima, Eo_prima, sigma_prima, alpha_prima)
@@ -68,7 +62,7 @@ for i in range(n_iteraciones-1):
 
     else:
         Beta = np.random.random()
-        if(np.log(Beta)<=Alpha):
+        if(log(Beta)<=Alpha):
             paso_A  = np.append(paso_A,A_prima)
             paso_B  = np.append(paso_B,B_prima)
             paso_Eo  = np.append(paso_Eo,Eo_prima)
@@ -84,12 +78,12 @@ for i in range(n_iteraciones-1):
             paso_alpha  = np.append(paso_alpha,paso_alpha[i])
             paso_L  = np.append(paso_L,L_inicial)
 
-max_Likelihood_id = np.argmin(paso_L)
-mejor_A = paso_A[max_Likelihood_id]
-mejor_B = paso_B[max_Likelihood_id]
-mejor_Eo = paso_Eo[max_Likelihood_id]
-mejor_sigma = paso_sigma[max_Likelihood_id]
-mejor_alpha = paso_alpha[max_Likelihood_id]
+max_Likelihood = np.argmin(paso_L)
+mejor_A = paso_A[max_Likelihood]
+mejor_B = paso_B[max_Likelihood]
+mejor_Eo = paso_Eo[max_Likelihood]
+mejor_sigma = paso_sigma[max_Likelihood]
+mejor_alpha = paso_alpha[max_Likelihood]
 
 print "El valor de A es", mejor_A
 print "El valor de B es", mejor_B
@@ -97,17 +91,23 @@ print "El valor de E_0 es", mejor_Eo
 print "El valor de sigma es", mejor_sigma
 print "El valor de alpha es", mejor_alpha
 
-plt.scatter(n_ener_datos,n_cuen_datos)
-plt.plot(n_ener_datos,mi_modelo(n_ener_datos, mejor_A, mejor_B, mejor_Eo, mejor_sigma, mejor_alpha))
+plt.scatter(n_ener_datos,n_cuen_datos,color='green',label='Datos originales')
+plt.plot(n_ener_datos,mi_modelo(n_ener_datos, mejor_A, mejor_B, mejor_Eo, mejor_sigma, mejor_alpha),color='blue',label='Datos estimados')
+plt.legend()
 plt.show()
 
 count, bins, ignored =plt.hist(paso_A, 20, normed=True)
+plt.title('A')
 plt.show()
 count, bins, ignored =plt.hist(paso_B, 20, normed=True)
+plt.title('B')
 plt.show()
 count, bins, ignored =plt.hist(paso_Eo, 20, normed=True)
+plt.title('Eo')
 plt.show()
 count, bins, ignored =plt.hist(paso_alpha, 20, normed=True)
+plt.title('alpha')
 plt.show()
 count, bins, ignored =plt.hist(paso_sigma, 20, normed=True)
+plt.title('sigma')
 plt.show()
